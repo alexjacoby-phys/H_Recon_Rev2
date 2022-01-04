d5e36704ed72695a4c8dc0411cc43cdc2f8bb077
Alexander Jacoby <alex_jacoby@brown.edu>
2022-01-04 00:21:36 -0500

    Upodate before branching.

--- DMRG_v2.1/Runme.jl
+++ DMRG_v2.1/Runme.jl
@@ -2,24 +2,15 @@ include("Hamiltonian-maker.jl")
 
 
 Ns = 20
-H = XVBSmake(Ns,0.1,0.)
+θ = 0.3
+H = XVBSmake(Ns,cos(θ),0.25*sin(θ))
 psi = makePsi0(Ns)
 
-for i in 1:80
-    @time dmrg(psi, H, maxm = 10, cutoff = 1E-4, noise = 1E-5)
-end
+dmrg(psi, H, maxm = 10, cutoff = 1E-3, sweeps = 100)
 print("coarse run completed")
-for i in 1:20
-    @time dmrg(psi, H, maxm = 40, cutoff = 1E-8)
-end
+dmrg(psi, H, maxm = 50, cutoff = 1E-8, sweeps = 20, cvgE = false)
 print("intermediate run completed")
-for i in 1:10
-    @time dmrg(psi, H, maxm = 80, cutoff = 1E-15)
-end
+dmrg(psi, H, maxm = 100, cutoff = 1E-12, sweeps = 10, cvgE = false)
 print("fine run completed")
 variance = expect(psi,H,H)- (expect(psi, H))^2
 print("variance is, ",variance)
-
-psi[10][]
-move!(psi, 5)
-svd(psi)
